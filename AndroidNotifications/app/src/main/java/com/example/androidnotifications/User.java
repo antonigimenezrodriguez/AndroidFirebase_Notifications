afff3e4708e826417c69f3f6a31e6974abf2a1e8
Antoni Giménez <angirod3@upv.es>
2021-03-05 15:26:22 +0100

    Cosntructor User

--- AndroidNotifications/app/src/main/java/com/example/androidnotifications/User.java
+++ AndroidNotifications/app/src/main/java/com/example/androidnotifications/User.java
@@ -3,5 +3,10 @@ package com.example.androidnotifications;
 public class User {
     public String email;
     public String token;
+
+    public User(String email, String token) {
+        this.email = email;
+        this.token = token;
+    }
 }
 
