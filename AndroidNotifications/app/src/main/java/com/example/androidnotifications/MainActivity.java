8b1208d7ed82e5d058330b1f585ff5e2b068449a
Antoni Giménez <angirod3@upv.es>
2021-03-05 13:51:07 +0100

    Crear Proyecto Android Studio Mostrat Notificación Simple Token Firebase Cloud Messaging

---
+++ AndroidNotifications/app/src/main/java/com/example/androidnotifications/MainActivity.java
@@ -0,0 +1,68 @@
+package com.example.androidnotifications;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.iid.FirebaseInstanceId;
+import com.google.firebase.iid.InstanceIdResult;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String CHANNEL_ID = "notifications";
+    private static final String CHANNEL_NAME = "Notifications Channel";
+    private static final String CHANNEL_DESC = "Notifications in android";
+
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, CHANNEL_NAME, NotificationManager.IMPORTANCE_DEFAULT);
+            channel.setDescription(CHANNEL_DESC);
+            NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+
+        textView = findViewById(R.id.textViewToken);
+
+        FirebaseInstanceId.getInstance().getInstanceId()
+                .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<InstanceIdResult> task) {
+                        if (task.isSuccessful()) {
+                            String token = task.getResult().getToken();
+                            textView.setText("Token : " + token);
+                        } else {
+                            textView.setText("Token not generated");
+                        }
+                    }
+                });
+
+    }
+
+    private void displayNotification() {
+        NotificationCompat.Builder mBuilder = new NotificationCompat.Builder(this, CHANNEL_ID)
+                .setSmallIcon(R.drawable.ic_notification)
+                .setContentTitle("The notification is working!!")
+                .setContentText("This is a text of a notification")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+        NotificationManagerCompat mNotificationMgr = NotificationManagerCompat.from(this);
+        mNotificationMgr.notify(1, mBuilder.build());
+    }
+}
