a2cd6ba8e602612ecf5c68aaa3a373a49a871eef
Antoni Giménez <angirod3@upv.es>
2021-03-05 15:36:36 +0100

    Guardamos el token

--- AndroidNotifications/app/src/main/java/com/example/androidnotifications/ProfileActivity.java
+++ AndroidNotifications/app/src/main/java/com/example/androidnotifications/ProfileActivity.java
@@ -4,18 +4,26 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.InstanceIdResult;
 
 public class ProfileActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+
+        mAuth = FirebaseAuth.getInstance();
 
         FirebaseInstanceId.getInstance().getInstanceId()
                 .addOnCompleteListener(new OnCompleteListener<InstanceIdResult>() {
@@ -23,11 +31,33 @@ public class ProfileActivity extends AppCompatActivity {
                     public void onComplete(@NonNull Task<InstanceIdResult> task) {
                         if (task.isSuccessful()) {
                             String token = task.getResult().getToken();
-
+                            saveToken(token);
                         } else {
 
                         }
                     }
                 });
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    private void saveToken(String token){
+        String email = mAuth.getCurrentUser().getEmail();
+        User user = new User(email, token);
+
+        DatabaseReference dbUsers = FirebaseDatabase.getInstance().getReference("users");
+        dbUsers.child(mAuth.getCurrentUser().getUid())
+                .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if(task.isSuccessful()){
+                    Toast.makeText(ProfileActivity.this, "Token Saved", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+    }
 }
+
